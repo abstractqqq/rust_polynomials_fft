a8a29fe0d69431e4279112ccb1de69fbe6bbd37e
tq <qtren2015@hotmail.com>
2022-11-29 18:02:24 -0500

    tiny refactor

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,48 @@
+use itertools::{EitherOrBoth::*, Itertools};
 
 struct Polynomial {
-    coeffs: Vec<f64>,
-    deg: usize
+    coeffs: Vec<f64>
 }
 
 impl Polynomial {
     fn new(mut c:Vec<f64>) -> Polynomial {
+        // do this later.
+        // if c.is_empty() {
+        //     panic!("Polynomial cannot be");
+        // }
 
-        loop {
-            match c.last(){
-                Some(v) => {
-                    if v == &0. {
-                        c.pop();
-                    } else {
-                        break
-                    }
-                }
-                _ => break
+        // For a polynomial, if we have 0x^5, then we should remove this term, except
+        // when 0 is the constant term, which happens when c.len() == 1.        
+        while c.len() > 1 {
+            let last = *c.last().unwrap();
+            if last == 0. {
+                c.pop();
+            } else {
+                break
             }
         }
-        let n = (c.len() - 1).max(0); // if c is empty, return 0
-        Polynomial {coeffs: c, deg: n}
+
+        Polynomial{coeffs: c}
+    }
+
+    fn add(&self, p:&Polynomial) -> Polynomial {
+        let mut new_poly:Vec<f64> = Vec::with_capacity(self.coeffs.len().max(p.coeffs.len()));
+        for pair in self.coeffs.iter().zip_longest(p.coeffs.iter()) {
+            match pair {
+                Both(l,r) => new_poly.push(l+r),
+                Left(l) => new_poly.push(*l),
+                Right(r) => new_poly.push(*r)
+            }
+        }
+        // in the minus case, it is possible that the leading term becomes 0
+        // so we use new to deal with that case
+        Polynomial::new(new_poly)
+
+    }
+
+    fn minus(&self, p:&Polynomial) -> Polynomial {
+        let p2 = Polynomial{coeffs: p.coeffs.iter().map(|x| -x).collect()};
+        self.add(&p2)
     }
 
 }
@@ -29,26 +51,37 @@ impl std::fmt::Display for Polynomial {
     
     fn fmt(&self, f:&mut std::fmt::Formatter) -> std::fmt::Result {
         let l = self.coeffs.len();
+        if l == 0 {
+            return write!(f, "Empty Polynomial. (Empty coefficients)")
+        }
         let mut p = String::new();
-        for i in (0..l).rev() {
-            if self.coeffs[i] == 0.0 {
-                continue;
+        for (i,coef) in self.coeffs.iter().enumerate().rev() {
+            if *coef == 0.0 {
+                if i == 0 && p.len() == 0 {
+                    p.push('0');
+                }    
+                continue
             }
             let mut signed = String::new();
             if i == l - 1 { // leading term, don't add sign
-                signed.push_str(&self.coeffs[i].to_string());
+                if *coef != 1. {
+                    signed.push_str(&coef.to_string());
+                }
             } else { // not leading term, add sign and use abs
                 if self.coeffs[i] < 0.0 {
                     signed.push_str(&" - ");
                 } else {
                     signed.push_str(&" + ");
                 }
-                signed.push_str(&self.coeffs[i].abs().to_string());
+                if i == 0 || *coef != 1. {
+                    // always push constants
+                    // No need to push 1 in front of x
+                    signed.push_str(&coef.abs().to_string());
+                }
             }
-
             if i > 0 {
                 signed.push('x');
-            } 
+            }
             if i > 1 {
                 signed.push('^');
                 signed.push_str(&i.to_string());
@@ -62,7 +95,10 @@ impl std::fmt::Display for Polynomial {
 
 
 fn main() {
-    let p1 = vec![-0.5,1.,-2.,-3.];
-    let p = Polynomial { coeffs: p1, deg: 3};
-    println!("{}", p);
+    let p1 = vec![0.5,1.,-2.,-3.];
+    let p2 = vec![0.5,-1.,3.,4.];
+    //let p2 = vec![0., -1., 2., 3.];
+    let q1 = Polynomial {coeffs: p1};
+    let q2 = Polynomial{coeffs: p2};
+    println!("{}", q1.add(&q2));
 }
